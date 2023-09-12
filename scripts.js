405bfaedc35bf3df9070012ee413599aeed34835
fundiculous <fundiculous@hotmail.com>
2023-09-11 23:17:35 -0500

    final commit

--- scripts.js
+++ scripts.js
@@ -18,15 +18,27 @@ function displayNews(articles) {
     const newsDiv = document.querySelector('#news');
     for (const article of articles) {
         const articleDiv = document.createElement('div');
+        articleDiv.classList.add('article'); 
 
         //create and append a headline to the articleDiv
         const title = document.createElement('h4');
-        title.textContent = article.title;
+        title.textContent = article.title.toUpperCase();
         articleDiv.appendChild(title);
+
+        //adding the article image
+        const articleImage = document.createElement('img');
+        articleImage.src = article.urlToImage;
+        articleImage.width = 220;
+        articleImage.alt = article.title; 
+        articleDiv.appendChild(articleImage);
 
         //create and append button to link to the article to read
         const linkToArticle = document.createElement('button');
-        linkToArticle.
+        linkToArticle.textContent = 'Full story here';
+        linkToArticle.target = '_blank'; 
+        articleDiv.appendChild(linkToArticle);
+
+   
     
       // TODO: Use document.createElement and appendChild to create and append more elements
   
